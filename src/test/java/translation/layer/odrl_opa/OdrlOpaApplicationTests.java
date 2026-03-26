f2216f678428d9f0bef598ecc3154f9e81ee70dd
cathoen <c14220146@john.petra.ac.id>
2026-03-26 15:17:04 +0100

    feat: spring initializr

---
+++ src/test/java/translation/layer/odrl_opa/OdrlOpaApplicationTests.java
@@ -0,0 +1,13 @@
+package translation.layer.odrl_opa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdrlOpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
